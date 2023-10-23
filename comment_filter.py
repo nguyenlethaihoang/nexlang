4e648e7e8d2d118edbb2384471c6c5a6b32851e0
nguyenlethaihoang <tinhoangadl@gmail.com>
2023-10-24 00:38:53 +0700

    update filter comment function

--- comment_filter.py
+++ comment_filter.py
@@ -1,42 +1,28 @@
 import re
 
+# Khả năng mở rộng cho filter_comments: Thay vì dựa vào số lượng ký tự bình luận để xác định cách lọc, hãy cố gắng thiết kế một cơ chế linh hoạt hơn. Ví dụ: bạn có thể định nghĩa cấu trúc dữ liệu cho từng loại bình luận (single-line, block start, block end) và xử lý dựa trên đó.
+
 
 def filter_comments(lines, language, language_comments):
-    comment_symbols = language_comments.get(language, [])
-    if not comment_symbols:
-        return lines
+    comment_data = language_comments.get(language, {"single": [], "block": []})
+    single_comments = comment_data["single"]
+    block_comments = comment_data["block"]
 
-    if len(comment_symbols) == 1:
-        single_line_comment = re.compile(
-            r'^\s*' + re.escape(comment_symbols[0]))
-        return [line for line in lines if not single_line_comment.match(line)]
+    filtered_lines = []
+    in_block_comment = False
+    end_block_comment = None if not block_comments else block_comments[1]
 
-    elif len(comment_symbols) == 2:
-        single_line_comment = re.compile(
-            r'^\s*' + re.escape(comment_symbols[0]) + '|' + re.escape(comment_symbols[1]) + '\s*$')
-        return [line for line in lines if not single_line_comment.match(line)]
+    for line in lines:
+        if not in_block_comment:
+            if any(line.strip().startswith(sym) for sym in single_comments):
+                continue
+            if block_comments and block_comments[0] in line:
+                in_block_comment = True
+                line = re.sub(re.escape(block_comments[0]) + '.*$', '', line)
+        if end_block_comment and end_block_comment in line:
+            in_block_comment = False
+            line = re.sub('^.*?' + re.escape(end_block_comment), '', line)
+        if not in_block_comment and line.strip() != "":
+            filtered_lines.append(line)
 
-    elif len(comment_symbols) == 3:
-        start_block_comment = re.escape(comment_symbols[1])
-        end_block_comment = re.escape(comment_symbols[2])
-        single_line_comment = re.compile(
-            r'^\s*' + re.escape(comment_symbols[0]) + '|' + end_block_comment + '\s*$')
-        block_comment = re.compile(
-            start_block_comment + '.*?' + end_block_comment, re.DOTALL)
-
-        filtered_lines = []
-        in_block_comment = False
-        for line in lines:
-            if not in_block_comment:
-                if single_line_comment.match(line):
-                    continue
-                if start_block_comment in line:
-                    in_block_comment = True
-                    line = re.sub(start_block_comment + '.*$', '', line)
-            if end_block_comment in line:
-                in_block_comment = False
-                line = re.sub('^.*?' + end_block_comment, '', line)
-            if not in_block_comment:
-                filtered_lines.append(line)
-
-        return filtered_lines
+    return filtered_lines
