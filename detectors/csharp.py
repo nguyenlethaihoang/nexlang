b9d6f9c2d862ac4e62a71bc7032e25944b26501e
nguyenlethaihoang <tinhoangadl@gmail.com>
2023-10-25 12:30:04 +0700

    Update C# Framework

--- detectors/csharp.py
+++ detectors/csharp.py
@@ -1,4 +1,8 @@
 def detect_csharp_framework(file_content):
     if "<TargetFramework>netcoreapp" in file_content or "<TargetFramework>netstandard" in file_content:
         return ".NET Core"
+    elif "<TargetFramework>netstandard" in file_content:
+        return ".NET Standard"
+    elif "<TargetFramework>net" in file_content:
+        return ".NET 5/6/7"
     return ".NET Framework"
